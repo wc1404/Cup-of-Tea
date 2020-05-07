d1f641b90ddf9b605a7120b1b4976670d25a5a3c
nivvy balakumar <nivvy@nyu.edu>
2020-05-06 21:33:21 -0400

    Update front end

---
+++ cupOfTea/cupOfTea/addPreferences.h
@@ -0,0 +1,17 @@
+//
+//  addPreferences.h
+//  cupOfTea
+//
+//  Created by nivvy balakumar on 5/6/20.
+//  Copyright © 2020 Nivvy Balakumar, Wilfredo Cueto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface addPreferences : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cupOfTea/cupOfTea/addPreferences.m
@@ -0,0 +1,32 @@
+//
+//  addPreferences.m
+//  cupOfTea
+//
+//  Created by nivvy balakumar on 5/6/20.
+//  Copyright © 2020 Nivvy Balakumar, Wilfredo Cueto. All rights reserved.
+//
+
+#import "addPreferences.h"
+
+@interface addPreferences ()
+
+@end
+
+@implementation addPreferences
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
